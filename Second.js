d367fec675353ad9e0ed03f5b752f0ac9fe46f07
jsanc042-boop <jsanc042@plattsburgh.edu>
2026-03-04 23:40:31 -0500

    Updated Second.js

--- Second.js
+++ Second.js
@@ -1,9 +1,22 @@
 const runGame = function() {
     // Your code additions go here
-
-
+let team1 = prompt("Lakers:");
+let score1 = parseInt(prompt("100:");
   
-  
+let team2 = prompt("Celtics:");
+let score2 = parseInt(prompt("95" + team2 + ":"));
+
+let result;
+
+if (score1 > score2) {
+    result = team1 + " won " + score1 + " to " + score2;
+}
+else if (score2 > score1) {
+    result = team2 = " won " + score2 + "to " + score1;
+}
+else {
+    result = "The game was a tie; " + score1 + " to " + score2;
+}
     // Code that adds a new paragraph with a string to the "div" in the HTML file 
     const divId = document.getElementById("results");
     const newPara = document.createElement("p");
